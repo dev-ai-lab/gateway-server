7407bbf60171429919495c6c4138feba6520bd88
enayetullahrasuli <enayetullah.rasuli@gmail.com>
2024-02-10 21:30:53 +0100

    Documentation of sql knowledge, cleanup, camunda integration

--- src/main/java/com/bank/gatewayserver/config/KeycloakRoleConverter.java
+++ src/main/java/com/bank/gatewayserver/config/KeycloakRoleConverter.java
@@ -5,7 +5,10 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
